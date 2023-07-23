c8e82815bdfe5c7c45c65bc943588390c073ce11
Hoang95vn <kaiser95vn@gmail.com>
2023-07-23 20:50:14 +0700

    update tag Name for EC2 and images

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -7,10 +7,18 @@ resource "aws_instance" "Udacity-T2" {
   ami           = "ami-04823729c75214919" # Amazon Linux 2 Kernel 5.10 AMI 2.0.20230628.0 x86_64 HVM gp2
   instance_type = "t2.micro"
   count         = 4
+  tags = {
+    Name        = "Udacity T2"
+    "Terraform" = "Yes"
+  }
 }
 
 resource "aws_instance" "Udacity-M4" {
   ami           = "ami-04823729c75214919" # Amazon Linux 2 Kernel 5.10 AMI 2.0.20230628.0 x86_64 HVM gp2
   instance_type = "m4.large"
   count         = 2
+  tags = {
+    Name        = "Udacity M4"
+    "Terraform" = "Yes"
+  }
 }
